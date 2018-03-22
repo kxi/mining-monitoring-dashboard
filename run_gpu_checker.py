59fb09ba0d03cc6ba40690cf9883257997074615
Kai Xi <kaixi.work@gmail.com>
2018-03-22 20:49:10 +1100

    Replace check_output() with call()

--- run_gpu_checker.py
+++ run_gpu_checker.py
@@ -9,10 +9,14 @@ def main():
     sleep_sec = interval_sec - timeout_sec
 
     try:
-        process = subprocess.check_output("python gpu_check.py {} {}".format(miner_id, interval_sec), stderr = subprocess.PIPE, timeout = timeout_sec)
+        process = subprocess.call("python gpu_check.py {} {}".format(miner_id, interval_sec), stderr=subprocess.STDOUT, timeout = timeout_sec)
+        # process = subprocess.Popen("python gpu_check.py {} {}".format(miner_id, interval_sec), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True, timeout = timeout_sec)
+        # output, error = process.communicate()
+
     except Exception as e:
         print(e)
 
+    print("Start Sleep")
     time.sleep(sleep_sec)
 
 main()
