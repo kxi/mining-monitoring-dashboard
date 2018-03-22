62fb1845113a012e822cb64ae67df17f851d300e
Kai Xi <kaixi.work@gmail.com>
2018-03-22 20:54:56 +1100

    Add Time Stamp

--- run_gpu_checker.py
+++ run_gpu_checker.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import time
+from datetime import datetime
 
 def main():
     miner_id = sys.argv[1]
@@ -8,15 +9,17 @@ def main():
     timeout_sec = 60
     sleep_sec = interval_sec - timeout_sec
 
+    print(str(datetime.now()))
+
     try:
         process = subprocess.call("python gpu_check.py {} {}".format(miner_id, interval_sec), stderr=subprocess.STDOUT, timeout = timeout_sec)
-        # process = subprocess.Popen("python gpu_check.py {} {}".format(miner_id, interval_sec), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True, timeout = timeout_sec)
-        # output, error = process.communicate()
 
     except Exception as e:
         print(e)
 
     print("Start Sleep")
     time.sleep(sleep_sec)
+
+    print(str(datetime.now()))
 
 main()
