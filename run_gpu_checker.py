9f8a8c9a7b1f9c8c3634129a364e591f6e81e72e
Kai Xi <kaixi.work@gmail.com>
2018-03-22 21:17:31 +1100

    Fixed the Loop

--- run_gpu_checker.py
+++ run_gpu_checker.py
@@ -9,24 +9,24 @@ def main():
     timeout_sec = 180
     sleep_sec = interval_sec - timeout_sec
 
-    print("###############################################################")
-    print("Start GPU Check @ {}".format((str(datetime.now()))))
-    now = time.time()
+    while 1:
+        print("###############################################################")
+        print("Start GPU Check @ {}".format((str(datetime.now()))))
+        now = time.time()
 
-    try:
-        process = subprocess.call("python gpu_check.py {} {}".format(miner_id, interval_sec), stderr=subprocess.STDOUT, timeout = timeout_sec)
+        try:
+            process = subprocess.call("python gpu_check.py {} {}".format(miner_id, interval_sec), stderr=subprocess.STDOUT, timeout = timeout_sec)
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
-    print("Finish GPU Check @ {}".format((str(datetime.now()))))
-    print("****** It Takes {0:.1f} Second to Complete GPU Check! ******".format(time.time() - now))
+        print("Finish GPU Check @ {}".format((str(datetime.now()))))
+        print("****** It Takes {0:.1f} Second to Complete GPU Check! ******".format(time.time() - now))
 
-    print("Start Sleep")
-    print("###############################################################")
-    print("")
+        print("Start Sleep")
+        print("###############################################################")
+        print("")
 
-    time.sleep(sleep_sec)
-
+        time.sleep(sleep_sec)
 
 main()
